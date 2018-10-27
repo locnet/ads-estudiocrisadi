939998ee7475a43b515ef8ccd65682a7cd51909b
Adrian Cojocaru <locnetarganda@gmail.com>
2018-10-27 18:42:56 +0200

    first commit

---
+++ empresa-reformas-madrid.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="es">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        
+        <meta name="description" content=" ">
+        <title>Estudio Crisadi</title>
+
+       
+
+        <!-- Fonts -->
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
+        <!-- Styles / Bootstrap import-->
+      
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+        <link href="main.css" rel="stylesheet">
+    
+
+    </head>
+
+    <body>
+        <div class="container-fluid title-container">
+            <div class="row">
+                <div class="col-12">
+                    <h1 class="text-center lato w-100 extra-big">HELLO WORLD</h1>
+                </div>
+            </div>
+        </div>
+    </body>
+
+    <!-- SCRIPTS -->
+   
+</html>
