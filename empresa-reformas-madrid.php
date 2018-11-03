fdab922b2b0396aa50cd12d924e16324e54ec206
Adrian Cojocaru <locnetarganda@gmail.com>
2018-11-03 19:46:19 +0100

    varios cambios en la imagen de la pagina de inicio

--- empresa-reformas-madrid.php
+++ empresa-reformas-madrid.php
@@ -9,31 +9,58 @@
         <meta name="description" content=" ">
         <title>Estudio Crisadi</title>       
 
-        <!-- Fonts -->
+        <!-- Icons -->
         <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
-        <!-- Styles / Bootstrap import-->
-      
+
+        <!-- Styles / Bootstrap import-->      
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 
+        <!-- Bootstrap overwrite   -->
         <link href="main.css" rel="stylesheet">   
 
     </head>
 
     <body>
-        <div class="container-fluid title-container">
-            <div class="row justify-content-center">
+        <div class="container-fluid">
+            <div class="row justify-content-center title-container">
                 <div class="col-12">
-                    <h1 class="text-center lato w-300 extra-big orange">HELLO WORLD</h1>
+                    <h1 class="text-center lato w-300 extra-big orange">REFORMAS INTEGRALES SIN COMPLICACIONES</h1>
                 </div>
                 <div class="col-12 text-center">
-                    <h2 class="lato w-400">
-                        <a href="#down" role="button" class="btn btn-lg btn-primary">MY COOL CTA</a>
+                    <h2 class="lato big w-400">
+                        <a href="#down" role="button" class="btn btn-lg btn-primary">QUIERO PRESUPUESTO</a>
                     </h2>
                 </div>
             </div>
-            <div class="row justify-content-center">
-                <div class="col-12">
-                    <h2 id="down">hola</h2>
+        </div>
+        <div class="container-fluid">
+            <!-- TESTIMONIALS -->
+            <div class="row justify-content-center  testimonials">
+                <div class="col-md-3 col-sm-4 col-xs-12">
+                    <div class="card">
+                        <img class="card-img-top" src="..." alt="Card image cap">
+                        <div class="card-body">
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-md-3 col-sm-4 col-xs-12">
+                    <div class="card">
+                        <img class="card-img-top" src="..." alt="Card image cap">
+                        <div class="card-body">
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-md-3 col-sm-4 col-xs-12">
+                    <div class="card">
+                        <img class="card-img-top" src="..." alt="Card image cap">
+                        <div class="card-body">
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
@@ -41,7 +68,7 @@
 
     <!-- SCRIPTS -->
         <!-- Smooth Scroll Efect -->
-        <script src="https://cdn.jsdelivr.net/gh/cferdinandi/smooth-scroll@14.0.0/dist/smooth-scroll.polyfills.min.js">
+        <script src="https://cdn.jsdelivr.net/gh/cferdinandi/smooth-scroll@14.0.0/dist/smooth-scroll.polyfills.min.js"></script>
         <script>
             var scroll = new SmoothScroll('a[href*="#"]',{
                 speed:1000,
